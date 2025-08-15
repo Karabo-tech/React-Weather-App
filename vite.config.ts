d0a9ff39942030140c18eefff0501400cdb01621
Karabo <karaboshabe226@gmail.com>
2025-08-16 01:27:09 +0200

    removing the process.env configirations

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   define: {
-      'process.env.VITE_OPENWEATHER_API_KEY' : JSON.stringify(process.env.VITE_OPENWEATHER_API_KEY),
-    },
+   
 })
